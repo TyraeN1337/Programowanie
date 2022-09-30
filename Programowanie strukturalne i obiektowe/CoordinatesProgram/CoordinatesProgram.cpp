bf626ea8852ecfd0d931e83100d5a6214303c12f
gutataw <skfafka>
2022-09-30 08:14:13 +0200

    sfd

--- Programowanie strukturalne i obiektowe/CoordinatesProgram/CoordinatesProgram.cpp
+++ Programowanie strukturalne i obiektowe/CoordinatesProgram/CoordinatesProgram.cpp
@@ -2,16 +2,26 @@
 #include <iostream>
 using namespace  std;
 
+struct Point
+{
+	int x;
+	int y;
+	int z;
+};
 void CoordinateTestVersion1()
 {
 	system("cls");
-	int x, y;
-	cout << "Podaj x: \n";
-	cin >> x;
-	cout << "Podaj y: \n";
-	cin >> y;
+	
+	Point UserPoint;
 
-	double distance = calculateDistance(x, y);
+	cout << "Podaj x: \n";
+	cin >> UserPoint.x;
+	cout << "Podaj y: \n";
+	cin >> UserPoint.y;
+	cout << "Podaj z: \n";
+	cin >> UserPoint.z;
+
+	double distance = calculateDistance(UserPoint.x,UserPoint.y);
 	cout << "Odleglosc to:  " << distance << '\n';
 }
 double calculateDistance(int a, int b)
@@ -20,6 +30,20 @@ double calculateDistance(int a, int b)
 }
 int main()
 {
-	CoordinateTestVersion1();
+	CoordinateTestVersion2();
 }
 
+void CoordinateTestVersion2()
+{
+	system("cls");
+	int x, y;
+
+	//Point point;
+	cout << "Podaj x: \n";
+	cin >> x;
+	cout << "Podaj y: \n";
+	cin >> y;
+
+	double distance = calculateDistance(x, y);
+	cout << "Odleglosc to:  " << distance << '\n';
+}
