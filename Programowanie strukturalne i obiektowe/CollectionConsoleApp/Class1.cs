2879efccb4365f908adba7f9b6eb5aefeae8d7ac
Uczen <uczen@ezse.pl>
2022-11-14 15:13:31 +0100

    ef

---
+++ Programowanie strukturalne i obiektowe/CollectionConsoleApp/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionConsoleApp
+{
+    class TestCollection
+    {
+        public void TestArrayCollection()
+        {
+            int[] arrayofnumber = new int[10];
+            arrayofnumber[5] = 2137;
+            var random = new Random();
+            for(int i = 0; i < arrayofnumber.Length; i++)
+            {
+                arrayofnumber[i] = random.Next() % 100;
+            }
+            Console.WriteLine("Liczby w tablicy: ");
+            foreach (int item in arrayofnumber)
+            {
+                Console.Write(item + " , ");
+            }
+            Console.WriteLine();
+            int max = arrayofnumber[0];
+            foreach(int number in arrayofnumber)
+            {
+                if (number > max)
+                    max = number;
+            }
+            Console.WriteLine("Max w tablicy to: " + max);
+        }
+    }
+}
